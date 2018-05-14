b298b248d1f9d3f27e9c7e893f2c9b932cc06a58
unknown <dianakuzuta777@gmail.com>
2018-05-14 22:59:19 +0300

    screenshots added

--- protractor.conf.js
+++ protractor.conf.js
@@ -17,10 +17,18 @@ exports.config = {
     jasmineNodeOpts: {
         showColors: true, // Use colors in the command line report.
     },
-    onPrepare: function() {
+    onPrepare: function () {
         var AllureReporter = require('jasmine-allure-reporter');
         jasmine.getEnv().addReporter(new AllureReporter({
             resultsDir: 'allure-results'
         }));
+        jasmine.getEnv().afterEach(function(done){
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            })
+        });
     }
 };
